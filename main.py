0eefa910940e4b4dcd1419b3bd5edb0262d9c596
jokersus <lolivampireslave@gmail.com>
2023-05-09 01:06:56 +0300

    Switched output to csv format

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 
         anilist_usernames.append(match)
 
-    anilist_users = {user_id: u for u in anilist_usernames if (user_id := anilist.get_user_id(u))}
+    anilist_users = {user_id: u for u in anilist_usernames if u and (user_id := anilist.get_user_id(u))}
     anilist_media = {int(re.search(r'anime/(\d+)/', link).group(1)): link for link in anilist_links}
 
     anilist_media_ids = list(anilist_media.keys())
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         possible_media = anilist_media_ids.difference((x[0] for x in media_list))
 
         if len(possible_media) == 0:
-            print(f'Nothing to recommend to {anilist_users[user_id]}')
+            print(f'{anilist_users[user_id]},*Nothing*')
             continue
 
-        print(f'{anilist_media[random.choice(list(possible_media))]} for {anilist_users[user_id]}')
+        print(f'{anilist_users[user_id]},{anilist_media[random.choice(list(possible_media))]}')
