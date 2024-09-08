418c207cb504b42a1cfc18460337deb48fade00c
David Camp <Bpendragon@users.noreply.github.com>
2024-09-07 19:47:21 -0700

    Re-write basically all logic, was way more in depth than I was expecting.

--- anilist.py
+++ anilist.py
@@ -1,65 +1,54 @@
 import sys
 import time
+from collections import defaultdict
 from typing import NamedTuple
 
 import requests
 from mezmorize import Cache
 
+DEFAULT_CONTRACT_TYPE = "S"
+
 
 class User(NamedTuple):
+    """
+    An anicord user.
+    Attributes:
+        username (str): The username of the user.
+        flag (str): Flag for contracts type.
+            Valid values: `S` (for Staff/Veteran Specials), `T` (for Trash Specials), `B` (for Both).
+            The parser will use DEFAULT_CONTRACT_TYPE if a value is not specified in the file.
+        user_id: The users id on Anilist.
+    """
     username: str
     flag: str
     user_id: int
 
 
 class AnilistEntry(NamedTuple):
+    """
+    An entry on Anilist
+    Attributes:
+        item_id (int): The item id on Anilist
+        url (str): Canonical URL of the item.
+        jp_title (str): The title of the item in romaji.
+        en_title (str): The title of the item in English.
+        isAnime: Whether the item is an anime (true) or manga (false).
+        isTrash: Whether the entry is for Trash Specials (true) or Staff/Veteran Specials (false).
+        isLongAnime: Whether the anime is 16+ Episodes, should always be false if isAnime is false.
+    """
+    item_id: int
     url: str
+    jp_title: str
+    en_title: str
+    isAnime: bool
     isTrash: bool
+    isLongAnime: bool
 
 
 # Cache for 1 year because the library hates me
 cache = Cache(CACHE_TYPE='filesystem', CACHE_DIR='cache', CACHE_DEFAULT_TIMEOUT=365 * 24 * 60 * 60)
 
 URL_BASE = 'https://graphql.anilist.co'
-
-
-def _make_request(query: str, variables: dict, timeout_seconds=5):
-    # retry on 429:
-    while True:
-        response = requests.post(url=URL_BASE, json={
-            'query': query,
-            'variables': variables
-        })
-
-        if response.status_code != 429:
-            break
-
-        print(f'429: {response}. Waiting {timeout_seconds} seconds...', file=sys.stderr)
-        time.sleep(timeout_seconds)
-        timeout_seconds *= 2
-
-    return response.json()
-
-
-def _get_all_pages(query, variables, *, query_field='mediaList', _page=0):
-    response = _make_request(query, variables={**variables, 'page': _page})
-
-    # Can make this asynchronous
-    data = response['data']['Page']
-    has_next_page = data['pageInfo']['hasNextPage']
-    query_list = data[query_field]
-
-    if has_next_page:
-        print(f'next {_page}')
-        return list([*query_list, *_get_all_pages(query, variables, _page=_page + 1)])
-
-    return list(query_list)
-
-
-def medialist_to_tuple(media_list):
-    media_list = filter(lambda m: m['status'] != 'PLANNING', media_list)
-    return list(map(lambda m: (m['media']['id'], m['media']['title']['userPreferred'], m['media']['type'], m['media']['episodes']), media_list))
-
 
 GET_USER_LIST_QUERY = """
 query($userName: String, $page: Int) {
@@ -82,11 +71,45 @@ query($userName: String, $page: Int) {
 }
 """
 
+GET_MEDIA_INFORMATION_query = """
+query ($page: Int, $mediaIds: [Int]) {
+  Page(page: $page, perPage: 50) {
+    pageInfo {
+      hasNextPage
+    }
+    media(id_in: $mediaIds) {
+      id
+      siteUrl
+      episodes
+      type
+      title {
+        romaji
+        english
+      }
+    }
+  }
+}
+"""
 
-@cache.memoize()
-def get_user_list(user_name: str) -> list:
-    return medialist_to_tuple(_get_all_pages(query=GET_USER_LIST_QUERY, variables={'userName': user_name}))
-
+#Reducing amount of data pulled to a bare minimum. This is all the data we need to make determinations.
+GET_MEDIA_IN_USERS_LIST_query = """
+query ($page: Int, $userIds: [Int], $mediaIds: [Int]) {
+  Page(page: $page, perPage: 50) {
+    pageInfo {
+      hasNextPage
+    }
+    mediaList(userId_in: $userIds, mediaId_in: $mediaIds) {
+      user {
+        id
+      }
+      status
+      media {
+        id
+      }
+    }
+  }
+}
+"""
 
 GET_USER_ID_QUERY = """
 query($userName: String) {
@@ -96,6 +119,50 @@ query($userName: String) {
 }
 """
 
+def _get_all_pages(query, variables, *, query_field='mediaList', _page=0):
+    response = _make_request(query, variables={**variables, 'page': _page})
+
+    # Can make this asynchronous
+    data = response['data']['Page']
+    has_next_page = data['pageInfo']['hasNextPage']
+    query_list = data[query_field]
+
+    if has_next_page:
+        print(f'next {_page}')
+        return list([*query_list, *_get_all_pages(query, variables, _page=_page + 1)])
+
+    return list(query_list)
+
+def _make_request(query: str, variables: dict):
+    # retry on 429 after time specified in the response:
+    while True:
+        response = requests.post(url=URL_BASE, json={
+            'query': query,
+            'variables': variables
+        })
+
+        if response.status_code != 429:
+            break
+
+        #Per the docs, going over the rate limit leads to a 1-minute timeout. a 429 should also have a `Retry-After` header.
+        #Timeout is set at 61 seconds to ensure that we wait at least the minimum time even if the header doesn't exist.
+        #If the header does exist we'll use the value provided by it.
+        #Both values are padded an extra 2 seconds to make sure we don't end up making the request just before the timeout is lifted.
+        timeout_seconds = 62
+        if "Retry-After" in response.headers:
+            timeout_seconds = int(response.headers["Retry-After"]) + 2
+
+        print(f'429: {response}. Waiting {timeout_seconds} seconds...', file=sys.stderr)
+        time.sleep(timeout_seconds)
+
+    return response.json()
+
+def get_media_users_are_ineligible_for(user_ids: [int], media_ids: [int]) -> defaultdict[int, [int]]:
+    # Sorting for caching
+    return _get_media_users_are_ineligible_for(sorted(user_ids), sorted(media_ids))
+
+def get_media_information(media_ids: [int]):
+    return _get_media_information(sorted(media_ids))
 
 @cache.memoize()
 def get_user_id(user: User) -> int | None:
@@ -110,52 +177,23 @@ def get_user_id(user: User) -> int | None:
     return response['data']['User']['id']
 
 
-GET_MISSING_MEDIA_query = """
-query($page: Int,  $userIds: [Int], $mediaIds: [Int]) {
-  Page(page: $page, perPage: 50) {
-    pageInfo {
-        hasNextPage
-    }
-    mediaList(userId_in: $userIds, mediaId_in: $mediaIds) {
-      user {
-        id
-        name
-      }
-      status
-      media {
-        status
-        id
-        episodes
-        type
-        title {
-          userPreferred
-        }
-      }
-    }
-  }
-}
-"""
-
-
-def get_missing_media(user_ids: [int], media_ids: [int]):
-    # Sorting for caching
-    return _get_missing_media(sorted(user_ids), sorted(media_ids))
+@cache.memoize()
+def _get_media_information(media_ids: [int]):
+    data = _get_all_pages(query=GET_MEDIA_INFORMATION_query, query_field="media", variables={'mediaIds': media_ids})
+    return data
 
 
 @cache.memoize()
-def _get_missing_media(user_ids: [int], media_ids: [int]):
-    data = _get_all_pages(query=GET_MISSING_MEDIA_query, variables={
+def _get_media_users_are_ineligible_for(user_ids: [int], media_ids: [int]) -> defaultdict[int, [int]]:
+    data = _get_all_pages(query=GET_MEDIA_IN_USERS_LIST_query, variables={
         'userIds': user_ids,
         'mediaIds': media_ids
     })
 
-    # Initialize with all user ids.
-    user_dict = {k: [] for k in user_ids}
-
-    for media_list in data:
-        user_dict[media_list['user']['id']].append(media_list)
-
-    for k, v in user_dict.items():
-        user_dict[k] = medialist_to_tuple(v)
+    # Default dicts are great.
+    user_dict = defaultdict(list)
+    for list_item in data:
+        if list_item["status"] != 'PLANNING': continue
+        user_dict[int(list_item['user']['id'])].append(int(list_item['media']['id']))
 
     return user_dict
--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from collections import defaultdict, OrderedDict
 from pathlib import Path
 
 import anilist
+from anilist import AnilistEntry
 
 USERNAMES_FILE_NAME = Path('./data/usernames.txt')
 POOL_FILE_NAME = Path('./data/pool.txt')
 
-selections = defaultdict(int)
-special_selections = defaultdict(int)
+anilist_media_information = dict[int, AnilistEntry]()
+anilist_users = OrderedDict() # We want to preserve the original insertion order, this means that older anilist users don't have an "advantage"
+staff_selections = defaultdict(int)
 trash_selections = defaultdict(int)
 
 def _parse_file(file_name: Path):
@@ -19,23 +21,50 @@ def _parse_file(file_name: Path):
         return data.split('\n')
 
 
-def select_anime(user_id: int, missing_media, media_list, avoid_long: bool = False, is_trash: bool = False):
-    possible_media = missing_media.difference((x[0] for x in media_list))
+def select_anime(possible_media: [int], is_trash: bool = False) -> int:
+    if len(possible_media) == 0: return -1
     choices = random.choices(list(possible_media), k=len(possible_media))
-    choice = min([(c, trash_selections[c] if is_trash else special_selections[c]) for c in choices], key=lambda x: x[1])
+
+    choice = choices[0]
+    for c in choices[1:]:
+        if is_trash and trash_selections[c] < trash_selections[choice]: choice = c
+        if not is_trash and staff_selections[c] < staff_selections[choice]: choice = c
+
     if is_trash:
-        trash_selections[choice[0]] += 1
-    else :
-        special_selections[choice[0]] += 1
+        trash_selections[choice] += 1
+    else:
+        staff_selections[choice] += 1
     return choice[0]
 
 if __name__ == '__main__':
     anilist_links = _parse_file(POOL_FILE_NAME)
     anilist_usernames_file = _parse_file(USERNAMES_FILE_NAME)
 
-    anilist_usernames = []
+    #Call anilist to get information about the anime we want to watch
+    anilist_isTrash = {} #Look, I know it's not pythonic and all that, but it allows me to save expensive API calls or cycling through the file list again.
+
+    for link in anilist_links:
+        anilistId = int(re.search(r'(?:anime|manga)/(\d+)/', link).group(1))
+        parts = link.partition(" | ")
+        anilist_isTrash[anilistId] = parts[2] == "T"
+
+    anilist_data = anilist.get_media_information(anilist_isTrash.keys())
+
+    for entry in anilist_data:
+        item_id = int(entry["id"]) #Pre-extracting only things that need read multiple times for human readability.
+        isAnime = entry["type"] == "ANIME"
+        isLongAnime = False if not isAnime else entry["episodes"] >= 16
+        anilist_media_information[item_id] = anilist.AnilistEntry(
+            item_id=item_id,
+            url=entry["siteUrl"],
+            jp_title=entry["title"]["romaji"],
+            en_title = entry["title"]["english"],
+            isAnime=isAnime,
+            isLongAnime=isLongAnime,
+            isTrash=anilist_isTrash[item_id])
+
+    #Get user ids for all participating members.
     for u in anilist_usernames_file:
-        # Simpler this way
         us = u.partition(" | ")
         if us[0].lower().startswith('https://'):
             match = re.search(r'(?<=user/)([a-zA-Z0-9]+)/?', u)
@@ -48,66 +77,103 @@ if __name__ == '__main__':
         else:
             match = us[0]
 
-        anilist_usernames.append(anilist.User(match, us[2], anilist.get_user_id(match)))
+        user_id = anilist.get_user_id(match)
+        if user_id is None:
+            print(f'Anilist user not found: {u}', file=sys.stderr)
+            continue
+        anilist_users[user_id] = anilist.User(match, us[2] if us[2] else anilist.DEFAULT_CONTRACT_TYPE, int(user_id))
 
-    # We want to preserve the original insertion order
-    anilist_users = OrderedDict((user_id, u) for u in anilist_usernames if u and (user_id := anilist.get_user_id(u)))
-
-    anilist_media = {}
-
-    for link in anilist_links:
-        anilistId = int(re.search(r'(?:anime|manga)/(\d+)/', link).group(1))
-        parts = link.partition(" | ")
-        anilist_media[anilistId] = anilist.AnilistEntry(parts[0], parts[2] == "T")
+    # We now have the background information to allow us to start assigning anime.
 
     # both_users exists to allow us to do some optimizations around who gets what and disallowing double 2-cours.
-    trash_users = sorted([u.user_id for u in anilist_usernames if u.flag in {"T", "B"}])
-    special_users = sorted([u.user_id for u in anilist_usernames if u.flag in {"S", "B"}])
-    both_users = sorted([u.user_id for u in anilist_usernames if u.flag == "B"])
+    # These are in insertion order because anilist_users is an OrderedDict
+    trash_users = [int(k) for k,v in anilist_users.items() if v.flag in {"T", "B"}]
+    staff_users = [int(k) for k,v in anilist_users.items() if v.flag in {"S", "B"}]
+    both_users = [int(k) for k,v in anilist_users.items() if v.flag == "B"]
 
+    trash_anime = set([k for k, v in anilist_media_information.items() if v.isTrash])
+    special_anime = set([k for k, v in anilist_media_information.items() if not v.isTrash])
 
-    trash_anime = [kvp[0] for kvp in anilist_media.items() if kvp[1].isTrash]
-    special_anime = [kvp[0] for kvp in anilist_media.items() if not kvp[1].isTrash]
-    # Anime users haven't seen
-    users_missing_staff_media = anilist.get_missing_media(user_ids=list(anilist_users.keys()), media_ids=special_anime)
-    users_missing_trash_media = anilist.get_missing_media(user_ids=trash_users, media_ids=trash_anime)
+    # Get the lists of media that the user has seen at least some of on Anilist
+    staff_media_users_ineligible_for = anilist.get_media_users_are_ineligible_for(user_ids=staff_users, media_ids=special_anime)
+    trash_media_users_ineligible_for = anilist.get_media_users_are_ineligible_for(user_ids=trash_users, media_ids=trash_anime)
 
-    users_assigned_specials = {}
-    users_assigned_trash = {}
+    #We can now generate lists for which anime the user _is_ eligible to be selected for.
+    staff_media_users_eligible_for = dict[int, [int]]() #Key is User ID, value is a list of Media IDs that user is eligible to be given.
+    trash_media_users_eligible_for = dict[int, [int]]()
 
-    # Users that signed up for both get priority because Bpen says so.
+    for u in staff_users:
+        staff_media_users_eligible_for[u] = list(special_anime.difference(staff_media_users_ineligible_for[u]))
+
+    for u in trash_users:
+        trash_media_users_eligible_for[u] = list(special_anime.difference(trash_media_users_ineligible_for[u]))
+
+    users_assigned_staff = dict[int, int]() #Key is User ID, Value is media ID, we'll then format the data out of the respective dicts at the end.
+    users_assigned_trash = dict[int, int]()
+
+    # Users that signed up for both get priority because Bpen says so (also because they're most likely to throw the selection balance out of whack).
 
     for user_id in both_users:
+        # First check both lists and see if one of them only contains long shows.
+        # If so, we'll select from that list first, and limit the other half to short shows.
+        # Note: a user can still receive two long shows if that's all they're eligible for.
+
+        elig_short_staff = [int(k) for k in staff_media_users_eligible_for[user_id] if not anilist_media_information[k].isLongAnime]
+        elig_short_trash = [int(k) for k in trash_media_users_eligible_for[user_id] if not anilist_media_information[k].isLongAnime]
+
+        # Flip a coin to see what will get assigned first
         trash_first = random.randint(0, 1) == 1
-        first_anime = None
+        force_two_long = False
+
+        # Override the coin if needed, if they're unlucky enough to be forced into two long shows we'll abide by the coin, not that it really matters
+        # How this works:
+        # If a user has eligible short entries on both sides, we abide by the coin.
+        #   If they pull a short show (or manga) in their first pull it uses their entire list for the second pull as well, they might end up with two short, who knows.
+        #   If they pull a long show on their first pull we reduce the pool to just short shows (and manga) for the second pull.
+        # If a user is going to be forced to have a long show for one type (but has eligible short/manga in the other) we force the pull on the long side first.
+        #   This then means the check for a long show is triggered, and we automatically use the short list on the other one
+        # If a User has no eligible short shows/manga on either side we set the `force_two_long` bool to true. So that when they are assigned their shows it doesn't try to give them nothing on the second one.
+        if len(elig_short_staff) == 0 and len(elig_short_trash) == 0: force_two_long = True
+        elif len(elig_short_staff) == 0: trash_first = False
+        elif len(elig_short_trash) == 0: trash_first = True
+
         if trash_first:
-            first_anime = select_anime(user_id, users_missing_trash_media[])
+            first_anime = select_anime(trash_media_users_eligible_for[user_id], True)
+            users_assigned_trash[user_id] = first_anime
+            second_anime = -1
+            if first_anime != -1 and anilist_media_information[first_anime].isLongAnime and not force_two_long:
+                second_anime = select_anime(elig_short_staff, False)
+            else:
+                second_anime = select_anime(staff_media_users_eligible_for[user_id], False)
+            users_assigned_staff[user_id] = second_anime
+        else:
+            first_anime = select_anime(staff_media_users_eligible_for[user_id], False)
+            users_assigned_staff[user_id] = first_anime
+            second_anime = -1
+            if first_anime != -1 and anilist_media_information[first_anime].isLongAnime and not force_two_long:
+                second_anime = select_anime(elig_short_trash, True)
+            else:
+                second_anime = select_anime(trash_media_users_eligible_for[user_id], True)
+            users_assigned_trash[user_id] = second_anime
 
-    for user_id, media_list in sorted(users_missing_media.items()):
-        possible_media = anilist_media_ids.difference((x[0] for x in media_list))
+    # That's the hard stuff out of the way. Now for the much easier steps of finishing assigning staff/veterans and then trash.
 
-        if len(possible_media) == 0:
-            print(f'{anilist_users[user_id]},*Nothing*')
-            continue
+    for u in staff_users:
+        if u in both_users: continue
+        anime_id = select_anime(staff_media_users_eligible_for[u], False)
+        users_assigned_staff[u] = anime_id
 
-        # Flatten the distribution
-        choices = random.choices(list(possible_media), k=len(possible_media))
-        choice = min([(c, selections[c]) for c in choices], key=lambda x: x[1])
-        selections[choice[0]] += 1
+    for u in trash_users:
+        if u in both_users: continue
+        anime_id = select_anime(trash_media_users_eligible_for[u], True)
+        users_assigned_trash[u] = anime_id
 
-        users_assigned_anime[user_id] = f'{anilist_users[user_id]},{anilist_media[choice[0]]}'
+    anilist_media_information[-1] = anilist.AnilistEntry(-1, "***NOTHING***", "***NOTHING***", "***NOTHING***", False, False, False)
 
-    print('\n'.join(users_assigned_anime.values()))
+    print("\nStaff/Veteran Specials:\n")
+    for user, media in users_assigned_staff.items():
+        print(f"{anilist_users[user].username}: \"{anilist_media_information[media].en_title}\" {'Anime' if anilist_media_information[media].isAnime else 'Manga'}")
 
-    print()
-    print('Stats:')
-
-    sorted_selections = sorted(selections.items(), key=lambda x: x[1], reverse=True)
-    selected_media = [anilist_media[k] for k in selections.keys()]
-
-    for k, c in sorted(selections.items()):
-        print(f'{anilist_media[k]}: {c}')
-
-    print(f'Total: {len(selections)}/{len(anilist_media)}')
-    print('Missing:')
-    print('\n'.join(list(set(anilist_media.values()).difference(selected_media))))
+    print("\n------------------------------------\nTrash Specials:\n")
+    for user, media in users_assigned_trash.items():
+        print(f"{anilist_users[user].username}: \"{anilist_media_information[media].en_title}\" {'Anime' if anilist_media_information[media].isAnime else 'Manga'}")
